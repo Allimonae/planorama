531e5fdeeaf0de8fa4a604beebea44c5d130f217
Allimonae <128328789+Allimonae@users.noreply.github.com>
2025-04-13 11:20:00 -0400

    working on event handling

--- frontend/src/pages/BookingForm.tsx
+++ frontend/src/pages/BookingForm.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import hunterCampus from '../assets/hunter-campus-2001415976.jpg'
 
 const BookingForm = () => {
     const rooms = ['Room 1', 'Room 2', 'Room 3'] // should only list rooms that are available
+    
+    // Times array given start and end time
     const times = (start:string, end:string) => {
         let res = [];
         const mins = ['00', '30'];
@@ -24,20 +27,22 @@ const BookingForm = () => {
 
     // Dropdown with array Dropdown
     interface DropdownProps {
+        id: string;
+        value: string;
         label: string;
         options: string[];
     }
     
     const Dropdown = (props: DropdownProps) => {
-        const { label, options } = props;
+        const { id, label, options, value } = props;
         return(
             <div className='booking-form-element text-lg flex-1'>
                 <label>{ label }</label>
                 <br/>
-                <select className='h-[35px]'>
+                <select id={id} name={id} value={value} className='h-[35px]'>
                     {options.map((option, index) => (
-                        <option key={ index } value={ option }>
-                            { option }
+                        <option key={index} value={option}>
+                            {option}
                         </option>
                     ))};
                 </select>
@@ -47,84 +52,125 @@ const BookingForm = () => {
 
     // Text input
     interface TextInputProps {
+        id: string;
+        value:
         label: string;
         type: string;
     }
 
     const TextInput = (props: TextInputProps) => {
-        const { label, type } = props;
+        const { id, label, type, value } = props;
         return (
             <div className='booking-form-element text-lg'>
-                <label>{ label }</label>
+                <label>{label}</label>
                 <br/>
-                <input className='h-[35px]' type={ type }></input>
+                {type === 'textarea' ? (
+                    <div>
+                        <textarea className='h-[200px]' 
+                            id={id}
+                            name={id}
+                            value={value}
+                        ></textarea>
+                    </div>
+                ) : (
+                    <div>
+                        <input className='h-[35px]' 
+                            id={id} 
+                            name={id}
+                            type={type}
+                        ></input>
+                    </div>
+                )}
             </div>
         )
     }
 
-    const submitBookingForm = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(times('2:00','14:00'));
+    const [bookingData, setBookingData] = useState({
+        room: '',
+        date: '',
+        start: '',
+        end: '',
+        // clubName: '',
+        // eventTitle: '',
+        // eventDescription: '',
+        // numGuests: '',
+        // dateCreated: ''
+    });
+
+    // Handle form input and validation
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // const {room, date, start, end} = e.target;
+        // setBookingData
     }
 
     return (
-        <div>
-            <form className='bg-amber-50 w-4/5 flex flex-col mx-auto my-[100px] py-[50px] rounded-4xl' 
-                id='booking-form'
-            >
+        <div className=''>
+            <div className='bg-amber-50 w-4/5 mx-auto my-24 flex flex-col py-[50px] rounded-3xl shadow-2xl'>
                 {/* HEADER */}
-                <h1 className='booking-form-element text-center text-4xl'>Reserve a room</h1>
-
-                {/* Room - dropdown */}
-                <Dropdown label='Select a room' 
-                    options={ rooms }/
+                <h1 className='booking-form-element text-center text-4xl font-bold'>Reserve a room</h1>
+                <form className='flex flex-col' 
+                    id='booking-form'
                 >
-                
-                {/* Date - how should date be handled? */}
-
-                {/* Time - dropdown, start time cannot be after end time, show duration */}
-                <div className='flex flex-col-2'> 
-                    {/* Start time */}
-                    <Dropdown label='Start Time' 
-                        options={ times('2:00', '14:00') }
-                    />
-
-                    {/* End time */}
-                    <Dropdown label='End Time' 
-                        options={ times('2:00', '14:00') }
+                    {/* Room - dropdown */}
+                    <Dropdown id='room'
+                        value={bookingData.room}
+                        label='Select a room' 
+                        options={ rooms }
                     />
                     
-                    {/* Duration - calculated from start time and end time */}
+                    {/* Date - how should date be handled? */}
 
-                </div>
+                    {/* Time - dropdown, start time cannot be after end time, show duration */}
+                    <div className='flex flex-col-2'> 
+                        {/* Start time */}
+                        <Dropdown id='start'
+                            value={bookingData.start}
+                            label='Start Time' 
+                            options={ times('2:00', '14:00') }
+                        />
 
-                {/* Club name - text */}
-                <TextInput label='Club Name' 
-                    type='text'
-                />
+                        {/* End time */}
+                        <Dropdown id='end'
+                            value={bookingData.end}
+                            label='End Time' 
+                            options={ times('2:00', '14:00') }
+                        />
+                        
+                        {/* Duration - calculated from start time and end time */}
 
-                {/* Event Title - text */}
-                <TextInput label='Event Title' 
-                    type='text'
-                />
+                    </div>
 
-                {/* Event Description - text area */}
-                <div className='booking-form-element text-lg'>
-                    <label>Event Description</label>
-                    <br/>
-                    <textarea className='h-[200px]'></textarea>
-                </div>
+                    {/* Club name - text */}
+                    <TextInput id='club-name'
+                        label='Club Name' 
+                        type='text'
+                    />
 
-                {/* Expected Number of Guests - number */}
-                <TextInput label='Expected Number of Guests' 
-                    type='number'
-                />
+                    {/* Event Title - text */}
+                    <TextInput id='event-title'
+                        label='Event Title' 
+                        type='text'
+                    />
 
-                {/* Tags */}
+                    {/* Event Description - text area */}
+                    <TextInput id='event-description'
+                        label='Event Description'
+                        type='textarea'
+                    ></TextInput>
 
-                {/* button - submit will tell you if booking was successful, and if not why (missing a field, time slot filled) */}
-                <button onClick={ submitBookingForm }>Submit</button>
-            </form>
+                    {/* Expected Number of Guests - number */}
+                    <TextInput id='no-of-guests'
+                        label='Expected Number of Guests' 
+                        type='number'
+                    />
+
+                    {/* Tags - optional */}
+
+                    {/* button - submit will tell you if booking was successful, and if not why (missing a field, time slot filled) */}
+                    <button onClick={ handleSubmit }>Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
