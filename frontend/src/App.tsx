7994aa70f17ed3597126f6e37452b518baf2bd87
Mackenzie Eng <86166064+makenzie9838@users.noreply.github.com>
2025-05-09 00:11:07 -0400

    Implement Clerk components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,31 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from "./pages/Calendar";
 import BookingForm from "./pages/BookingForm";
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 
 function App() {
   return (
     <>
+      <SignedIn>
       <BrowserRouter>
         <nav>
-          <Link to="/">Home</Link>
-          <Link to="/bookingform">Booking Form</Link>
+        <Link to="/">Home</Link>
+        <Link to="/bookingform">Booking Form</Link>
+        <UserButton />
         </nav>
 
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/bookingform" element={<BookingForm/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/bookingform" element={<BookingForm />} />
         </Routes>
       </BrowserRouter>
+      </SignedIn>
+      <SignedOut>
+      <div>
+        <h1>Welcome to the App</h1>
+        <SignInButton />
+      </div>
+      </SignedOut>
     </>
   );
 }
